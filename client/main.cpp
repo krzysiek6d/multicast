ed72ce914211bd7f66379ea0bfafdcec48b24882
krzysiek6d <krzysztof.pawluch@gmail.com>
2017-01-26 16:56:26 +0000

    client

---
+++ client/main.cpp
@@ -0,0 +1,75 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+int main(int argc, char *argv[])
+{
+	/* Create a datagram socket on which to receive. */
+	int sd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sd < 0)
+	{
+		perror("Opening datagram socket error");
+		exit(1);
+	}
+	else
+		printf("Opening datagram socket....OK.\n");
+	 
+	int reuse = 1;
+	if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+	{
+		perror("Setting SO_REUSEADDR error");
+		close(sd);
+		exit(1);
+	}
+	else
+		printf("Setting SO_REUSEADDR...OK.\n");
+	}
+	 
+	struct sockaddr_in localSock;
+	localSock.sin_family = AF_INET;
+	localSock.sin_port = htons(4321);
+	localSock.sin_addr.s_addr = INADDR_ANY;
+	if(bind(sd, (struct sockaddr*)&localSock, sizeof(localSock)))
+	{
+		perror("Binding datagram socket error");
+		close(sd);
+		exit(1);
+	}
+	else
+		printf("Binding datagram socket...OK.\n");
+	 
+	struct ip_mreq group;
+	group.imr_multiaddr.s_addr = inet_addr("226.1.1.1");
+	group.imr_interface.s_addr = inet_addr("192.168.1.9");
+	if(setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0)
+	{
+		perror("Adding multicast group error");
+		close(sd);
+		exit(1);
+	}
+	else
+		printf("Adding multicast group...OK.\n");
+	 
+	while(true)
+	{
+		char databuf[1024];
+		int datalen = sizeof(databuf);
+
+		if(read(sd, databuf, datalen) < 0)
+		{
+			perror("Reading datagram message error");
+			close(sd);
+			exit(1);
+		}
+		else
+		{
+			printf("Reading datagram message...OK.\n");
+			printf("The message from multicast server is: \"%s\"\n", databuf);
+		}
+	}
+	return 0;
+}
